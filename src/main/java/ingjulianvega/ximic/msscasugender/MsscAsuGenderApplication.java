5931277fc3fb8e7130f151492f7e9cde08c272f1
Julian Vega <ingjulianvega@gmail.com>
2021-04-04 18:46:19 -0500

    :zap: Initial commit. Closes #2 #3 #21 #22

---
+++ src/main/java/ingjulianvega/ximic/msscasugender/MsscAsuGenderApplication.java
@@ -0,0 +1,13 @@
+package ingjulianvega.ximic.msscasugender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscAsuGenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscAsuGenderApplication.class, args);
+	}
+
+}
