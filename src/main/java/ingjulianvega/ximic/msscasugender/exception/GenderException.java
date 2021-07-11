ad1fdf362ff57c749f6abd441fd90e0cdf48303e
Julian Vega <ingjulianvega@gmail.com>
2021-07-11 18:22:08 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasugender/exception/GenderException.java
+++ src/main/java/ingjulianvega/ximic/msscasugender/exception/GenderException.java
@@ -1,15 +1,19 @@
 package ingjulianvega.ximic.msscasugender.exception;
 
-import lombok.Getter;
+import lombok.*;
+import org.springframework.http.HttpStatus;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class GenderException extends RuntimeException {
 
-    private final String code;
-
-    public GenderException(final String code, final String message) {
-        super(message);
-        this.code = code;
-    }
+    private HttpStatus httpStatus;
+    private String apiCode;
+    private String error;
+    private String message;
+    private String solution;
 }
 
