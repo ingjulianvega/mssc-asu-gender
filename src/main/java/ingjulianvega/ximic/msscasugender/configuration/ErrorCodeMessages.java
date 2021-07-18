3cb515b2cbcd9f777d9ed958b89802da3c98c8de
Julian Vega <ingjulianvega@gmail.com>
2021-07-18 17:24:15 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasugender/configuration/ErrorCodeMessages.java
+++ src/main/java/ingjulianvega/ximic/msscasugender/configuration/ErrorCodeMessages.java
@@ -7,7 +7,7 @@ public class ErrorCodeMessages {
 
     public static final String GENDER_NOT_FOUND_API_CODE = "401";
     public static final String GENDER_NOT_FOUND_ERROR = "GENDER_NOT_FOUND";
-    public static final String GENDER_NOT_FOUND_MESSAGE = "No se encontró el estado civil";
+    public static final String GENDER_NOT_FOUND_MESSAGE = "No se encontró el género";
     public static final String GENDER_NOT_FOUND_SOLUTION = "Verifique los datos";
 
     public static final String ARGUMENT_NOT_VALID_API_CODE = "402";
