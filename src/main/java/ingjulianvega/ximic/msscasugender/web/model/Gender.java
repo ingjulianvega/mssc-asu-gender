39f29cf5f6d6272fa6751f7ccbb9e72558f4f043
Julian Vega <ingjulianvega@gmail.com>
2021-07-20 12:28:30 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasugender/web/model/Gender.java
+++ src/main/java/ingjulianvega/ximic/msscasugender/web/model/Gender.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Data
@@ -15,6 +16,7 @@ public class Gender implements Serializable {
 
     static final long serialVersionUID = 9198883824898848986L;
 
+    @NotBlank
     private String name;
 
 }
